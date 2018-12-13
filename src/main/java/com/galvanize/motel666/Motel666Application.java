7836a86078926015195d9fb58a940f5075701047
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-13 15:28:16 -0700

    create spring boot app
    core: DevTools, Lombok
    web: Web
    sql: Jpa, H2
    integration: RabbitMQ

---
+++ src/main/java/com/galvanize/motel666/Motel666Application.java
@@ -0,0 +1,14 @@
+package com.galvanize.motel666;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Motel666Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Motel666Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/galvanize/motel666/Motel666ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.galvanize.motel666;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Motel666ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
